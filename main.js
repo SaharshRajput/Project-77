f5843db1b954f87a5140d7025a9e21a6617f331a
SaharshRajput <73647463+SaharshRajput@users.noreply.github.com>
2021-05-07 18:07:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var a =10;
+var b=5;
+var addition=a+b;
+console.log(addition);
+
+var c =10;
+var d=5;
+var subtaction=c-d;
+console.log(subtraction);
+
+var  e=10;
+var f=5;
+var multiplication =e*f;
+console.log(multiplication);
+
+var g =10;
+var h=5;
+var division=g/h;
+console.log(division);
