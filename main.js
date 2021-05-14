aac7a610b11bb31ac195654c8b3006cdaba2306f
SaharshRajput <73647463+SaharshRajput@users.noreply.github.com>
2021-05-14 18:45:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-var a =10;
+var a=10;
 var b=5;
 var addition=a+b;
 console.log(addition);
 
-var c =10;
-var d=5;
-var subtaction=c-d;
+var subtraction=a-b;
 console.log(subtraction);
 
-var  e=10;
-var f=5;
-var multiplication =e*f;
+
+var multiplication =a*b;
 console.log(multiplication);
 
-var g =10;
-var h=5;
-var division=g/h;
+
+var division=a/b;
 console.log(division);
